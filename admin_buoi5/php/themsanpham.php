54fc5bb05a086c3173d4f9f7f75a5065e8483675
Tonion <nguyeencongthanh913@gmail.com>
2026-03-22 15:44:13 +0700

    trang san pham

---
+++ admin_buoi5/php/themsanpham.php
@@ -0,0 +1,25 @@
+
+<div class="Content">
+    <div class="TN_card-container">
+        <div class="TN_card-header">
+            Thêm sản phẩm
+        </div>
+        
+        <form method="POST" action="">
+            <div class="TN_card-body">
+                <div class="TN_form-group">
+                    <label for="tenSanPham">Tên sản phẩm:</label>
+                    <input type="text" id="tenSanPham" name="tenSanPham" placeholder="Nhập tên sản phẩm" required>
+                </div>
+                <div class="TN_form-group">
+                    <label for="thuTu">Thứ tự:</label>
+                    <input type="number" id="thuTu" name="thuTu" placeholder="Nhập số thứ tự" required>
+                </div>
+            </div>
+
+            <div class="TN_card-footer">
+                <button type="submit" class="TN_btn-submit">THÊM</button>
+            </div>
+        </form>
+    </div>
+</div>
