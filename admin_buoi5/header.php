4f053ebe8fa5514552f00d7a0b07e6e72a3ee27d
MrCoww2510 <mrcoww25102005@gmail.com>
2026-03-22 15:02:17 +0700

    thb5

---
+++ admin_buoi5/header.php
@@ -0,0 +1,41 @@
+<?php
+include("config.php")
+?>
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Quản lý hệ thống bán hàng</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+	<!-- ================= HEADER ================= -->
+	<div class="Header">
+		<img src="banner/co2.png" class="Co">
+		<img src="banner/co2.png" class="Co2">
+		<img src="banner/bang.png" class="Bang">
+		<h1 class="Header_Title">QUẢN LÝ HỆ THỐNG BÁN HÀNG</h1>
+	</div>
+
+	<!-- ================= MENU ================= -->
+	<div class="Menu">
+		<ul class="Menu_List">
+			<li><a href="#">Trang chủ</a></li>
+
+			<li class="Dropdown">
+				<a href="#">Thêm</a>
+				<ul class="Dropdown_Content">
+					<li><a href="#">Thêm sản phẩm</a></li>
+					<li><a href="#">Thêm nhóm</a></li>
+				</ul>
+			</li>
+
+			<li><a href="#">Cập nhật sản phẩm</a></li>
+			<li><a href="#">Cập nhật nhóm</a></li>
+
+			<li class="Right">Xin chào: họ và tên | <span class="Logout">Thoát</span></li>
+		</ul>
+	</div>
